abea4c49a3c12a4fac2d70c5bd3c2d7fd82af9d8
varb24 <bahamut246@gmail.com>
2022-10-21 12:16:55 -0400

    added spin direction
    Rotating platforms now have a boolean counterclockwise which decides spin direction.

--- Assets/Scripts/Platform Systems/RotatingPlatform.cs
+++ Assets/Scripts/Platform Systems/RotatingPlatform.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
-public class RotatingPlatform : MonoBehaviour 
+public class RotatingPlatform : BasePlatform 
 {
     public GameObject rotatingPlatform;
     public Vector3 startRotation;
     public int speed;
+    public bool counterClockwise;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +18,19 @@ public class RotatingPlatform : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Decides counterclockwise or clockwise rotation
+        if (counterClockwise) RotateCounterClockwise();
+        else RotateClockwise();
+
+    }
+
+    private void RotateClockwise(){
+        
+        transform.Rotate(Vector3.back, speed * Time.deltaTime);
+    }
+        
+    private void RotateCounterClockwise(){
+        
         transform.Rotate(Vector3.forward, speed * Time.deltaTime);
     }
 }
