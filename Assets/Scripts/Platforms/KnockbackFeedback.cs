c1bed095c8c14938a2b5ff154ba2c74d7f80577c
Osmany <58539319+SDOsmany@users.noreply.github.com>
2022-10-24 21:28:47 -0400

    Created the player knockback feature
    once placed on an object and the player collides with this object, the player will be knocked back

---
+++ Assets/Scripts/Platforms/KnockbackFeedback.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class KnockbackFeedback : MonoBehaviour
+{
+    [SerializeField] 
+    private Rigidbody2D rb2d;
+
+    [SerializeField] private GameObject playerObject;
+    
+    [SerializeField]
+    private float forceStrength = 10, delay = 0.15f;
+
+    public UnityEvent OnBegin, OnDone;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayFeedback(playerObject);
+        }
+    }
+
+    public void PlayFeedback(GameObject sender)
+    {
+        StopAllCoroutines();
+        OnBegin?.Invoke();
+        Vector2 direction = (sender.transform.position - transform.position).normalized;
+        rb2d.AddForce(direction * forceStrength, ForceMode2D.Impulse);
+        StartCoroutine(Reset());
+    }
+    
+    private IEnumerator Reset() 
+    {
+        yield return new WaitForSeconds(delay);
+        rb2d.velocity = Vector2.zero;
+        OnDone?.Invoke();
+    }
+}
