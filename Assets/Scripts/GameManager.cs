a75f7b7e2c00446721f6596b1e74a47adf10f0ad
PetersonNormil <petersonnormil@gmail.com>
2022-10-26 15:35:37 -0400

    starting game manager implementation
    basic set up, basic functionality to allow other members to participate and add extra features

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+/// <summary>
+/// The higher level game manager, will set the game flow/states and allow the game to pass any data or info.. 
+/// </summary>
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    [SerializeField] private GameState currentGameState; 
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+            Destroy(this);
+        else
+            Instance = this;
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        currentGameState = GameState.Base; 
+        //start clock 
+        //transition to 'base' level music state if needed 
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+    
+    //states 
+    //base 
+    //lvl 1 
+    //death 
+    //end game 
+}
+
+public enum GameState
+{
+    Base,
+    Level1,
+    Death,
+    EndGame
+}
