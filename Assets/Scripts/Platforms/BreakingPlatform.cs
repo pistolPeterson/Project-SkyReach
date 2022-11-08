844a9be42b2368f4cff70fcd28324b2e4ea37770
varb24 <39485630+varb24@users.noreply.github.com>
2022-11-08 18:05:42 -0500

    Testing disappearing platforms
    Platform currently disappears, needs method to call it back.

--- Assets/Scripts/Platforms/BreakingPlatform.cs
+++ Assets/Scripts/Platforms/BreakingPlatform.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 using DG.Tweening;
 public class BreakingPlatform : MonoBehaviour
 {
+    [SerializeField] GameObject breakingPlatform;
     public int timer;
+    int layerDefault;
     // Start is called before the first frame update
     void Start()
     {
-        
+        layerDefault = LayerMask.NameToLayer("Default");
     }
 
     // Update is called once per frame
@@ -18,7 +20,11 @@ public class BreakingPlatform : MonoBehaviour
     }
 
     private void OnCollisionEnter2D(Collision2D other){
-        Destroy(gameObject,timer);
-    }
 
+        //SpriteRenderer.color = new Color(1f,1f,1f,0f);
+       breakingPlatform.GetComponent<SpriteRenderer>().GetComponent<Renderer>().enabled = false;
+       breakingPlatform.GetComponent<Collider2D>().enabled = false;
+        Debug.Log("Current layer: " + breakingPlatform.layer);
+    }
 }
+
