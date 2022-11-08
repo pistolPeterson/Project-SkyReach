a83d908b35fa1c377a6eaae27cfbf76384290f07
Victor Martinez <vicplusplus@proton.me>
2022-11-08 17:12:13 -0500

    Create stun behaviour upon contact with object in layer "Knockback"

--- Assets/Scripts/Player/PlayerStun.cs
+++ Assets/Scripts/Player/PlayerStun.cs
@@ -8,29 +8,58 @@ using UnityEngine;
 
 public class PlayerStun : MonoBehaviour
 {
-    [SerializeField]private Animator animator;
+    [SerializeField] private Animator animator;
+    [SerializeField] private float stunTime = 1f;
+    [SerializeField] private float inputDelay = 0.5f;
+    [SerializeField] private float launchForce = 10f;
+    [SerializeField] private float launchAngleRange = 20f;
 
     private PlayerController controller;
-    // Start is called before the first frame update
-    void Start()
+
+    private void Awake()
     {
-        animator = GetComponent<Animator>();
         controller = GetComponent<PlayerController>();
-        StunPlayer();
+        animator = GetComponent<Animator>();
     }
-    
 
-    public void StunPlayer() //will be refactored to have variable stun time depending on severity of damage
+    // On collision enter, if the player collides with an object on layer "Knockback",
+    // the player will be stunned
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        if (animator == null)
+        if (other.gameObject.layer == LayerMask.NameToLayer("Knockback"))
         {
-            Debug.Log("Animator is null!");
-
-            return; 
+            StartCoroutine(Stun(-other.contacts[0].normal));
         }
+    }
+
+    // This coroutine will stun the player for stunTime seconds, and then
+    // apply an impulsive force to the player in the diagonal direction closest to the opposite of the normal of the collision,
+    // with some randomness added to the direction.
+    // The player will regain their movement after inputDelay seconds.
+    private IEnumerator Stun(Vector2 collisionNormal)
+    {
+        // Disable player movement
+        controller.enabled = false;
+        // freeze player in place
+        controller.Body.velocity = Vector2.zero;
         
-        animator.Play("StunAnimation");
-       
-        
+        animator.SetBool("Stunned", true);
+        yield return new WaitForSeconds(stunTime);
+
+        animator.SetBool("Stunned", false);
+
+        // Get the closest diagonal direction opposite to the collision normal
+        // if the horizontal component is zero, use player's last horizontal input
+        // if the horizontal component is not zero, use the horizontal component
+        float horizontal = collisionNormal.x == 0 ? controller.LastHorizontalFacingDirection.x : collisionNormal.x;
+        float vertical = collisionNormal.y;
+        Vector2 launchDirection = new Vector2(-Mathf.Sign(horizontal), -Mathf.Sign(vertical)).normalized;
+
+        // Apply launch force
+        controller.Body.AddForce(launchDirection * launchForce, ForceMode2D.Impulse);
+        yield return new WaitForSeconds(inputDelay);
+
+        // Re-enable player movement
+        controller.enabled = true;
     }
 }
