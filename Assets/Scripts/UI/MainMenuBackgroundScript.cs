8b6c31e18924c0ab4b63d70a0fdd0d543061ec4a
PetersonNormil <petersonnormil@gmail.com>
2022-11-12 13:15:33 -0500

    Testing main menu load up sfx

--- Assets/Scripts/UI/MainMenuBackgroundScript.cs
+++ Assets/Scripts/UI/MainMenuBackgroundScript.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 public class MainMenuBackgroundScript : MonoBehaviour
 {
     [SerializeField] private GameObject backgroundAnimLoadObj;
+    [SerializeField] private AudioClip loadUpSfx;
+    [SerializeField] private AudioSource _audioSource;
     public void HideBackground()
     {
         backgroundAnimLoadObj.gameObject.SetActive(false);
     }
 
+    public void PlayLoadSFX()
+    {
+        
+        _audioSource.PlayOneShot(loadUpSfx);
+    }
+    
 }
