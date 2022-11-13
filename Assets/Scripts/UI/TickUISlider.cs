b0ce631a49527402d777a18c614a5d58b4308154
PetersonNormil <petersonnormil@gmail.com>
2022-11-13 08:20:07 -0500

    Setting up a custom slider system

---
+++ Assets/Scripts/UI/TickUISlider.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TickUISlider : MonoBehaviour
+{
+    public List<GameObject> ticks;
+
+    private int index = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateTicks(); 
+    }
+    public void IncreaseTick()
+    {
+        if (index >= 9)
+        {
+            index = 9;
+            return;
+        }
+
+        index++;
+        UpdateTicks();
+    }
+    
+    public void DecreaseTick()
+    {
+        if (index <= 0)
+        {
+            index = 0; 
+            return;
+        }
+
+        index--;
+        UpdateTicks();
+    }
+    
+    private void UpdateTicks()
+    {
+        HideAllTicks();
+        for (int i = 0; i < index; i++)
+        {
+            ticks[i].SetActive(true);
+        }
+    }
+    
+    private void HideAllTicks()
+    {
+        foreach (var tick in ticks)
+        {
+            tick.SetActive(false);
+        }
+    }
+}
