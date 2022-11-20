73e7099e83c248c05eed4323a4322f204505bbf7
PetersonNormil <petersonnormil@gmail.com>
2022-11-20 14:53:45 -0500

    Starter code to carry Jaelene.
    Jaelene dont screw up.

---
+++ Assets/EndCreditsAnimation.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// make a summary for script dummy 
+/// </summary>
+public class EndCreditsAnimation : MonoBehaviour
+{
+    //reference to button gameobject 
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //make button not active on start 
+        
+        
+    }
+
+   
+    
+    //a method that sets the button gameobject active 
+    
+}
