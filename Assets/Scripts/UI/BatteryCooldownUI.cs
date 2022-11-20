66aa780e497661ccb74412cbe6f9bf2fd1e2a040
PetersonNormil <petersonnormil@gmail.com>
2022-11-20 07:32:52 -0500

    slightly clean up script
    remove animator in Player prefab
    
    Co-Authored-By: Raaee <99899402+Raaee@users.noreply.github.com>

--- Assets/Scripts/UI/BatteryCooldownUI.cs
+++ Assets/Scripts/UI/BatteryCooldownUI.cs
@@ -11,52 +11,39 @@ public class BatteryCooldownUI : MonoBehaviour
 
     [SerializeField] private Sprite[] batterySpriteLevels;//the levels of the battery from empty to full, we are garanteed 5 sprites
     [SerializeField] private SpriteRenderer sr;
-
-    [SerializeField] private Animator animator;
     private GrapplingHook grapplingHook;
-
-    private double fullCD, halfCD, thirdCD, fourthCD;
-
     
-    //TODO: Refactor so its going through the 5 sprite levels instead of the 2 I did below 
     private void Start()
     {
         grapplingHook = GetComponent<GrapplingHook>();
-        fullCD = grapplingHook.GetCooldownLength();
-        halfCD = grapplingHook.GetCooldownLength() / 2;
-        thirdCD = grapplingHook.GetCooldownLength() / 3;
-        fourthCD = grapplingHook.GetCooldownLength() / 4;
-
     }
 
     private void Update()
     {
-        Debug.Log(fullCD + " " + halfCD + " " + thirdCD + " " + fourthCD + " ");
-        Debug.Log(grapplingHook.GetCurrentCooldown());
-
-        // Must keep cooldownLength at 6 for this to look good in the game:
-        if (grapplingHook.GetCurrentCooldown() < fourthCD && grapplingHook.GetCurrentCooldown() > 0)
-            sr.sprite = batterySpriteLevels[4]; // full; 4 ticks >>> animator.Play("BatteryAnim");
-        else if (grapplingHook.GetCurrentCooldown() < thirdCD && grapplingHook.GetCurrentCooldown() > fourthCD)
-            sr.sprite = batterySpriteLevels[3]; // 3 ticks
-        else if (grapplingHook.GetCurrentCooldown() < halfCD && grapplingHook.GetCurrentCooldown() > thirdCD)
-            sr.sprite = batterySpriteLevels[2]; // 2 ticks
-        else if (grapplingHook.GetCurrentCooldown() < fullCD && grapplingHook.GetCurrentCooldown() > halfCD)
-            sr.sprite = batterySpriteLevels[1]; // 1 tick
-        else if (grapplingHook.GetCurrentCooldown() >= fullCD)
-            sr.sprite = batterySpriteLevels[0]; // empty
-        else if (grapplingHook.GetCurrentCooldown() <= 0)
-            sr.sprite = null; // full (not showing)
-
-
-        /* if (grapplingHook.GetCooldownLength() / 2 > grapplingHook.GetCurrentCooldown()) 
-         //if current cooldown is less than half (the timer in grappling hook is counting down)
-         {
-             sr.sprite = batterySpriteLevels[4]; // show full 
-         }
-         else
-         {
-             sr.sprite = batterySpriteLevels[0]; //show empty
-         }*/
+        //splits the battery cooldown into 5 sections and assigns the sprite depending on what current section it is in
+        //There are magic numbers, but we hace 5 sprites and the Battery cooldown can be removed
+        //from the grappling hook system with no compile errors 
+        if (grapplingHook.GetCurrentCooldown() <=  0)
+        {
+           sr.sprite = batterySpriteLevels[4]; 
+        }
+        else if (grapplingHook.GetCurrentCooldown() <  (grapplingHook.GetCooldownLength() * (0.2f)))
+        {
+            sr.sprite = batterySpriteLevels[3]; 
+        }
+        else if (grapplingHook.GetCurrentCooldown() < (grapplingHook.GetCooldownLength() * (0.4f)))
+        {
+            sr.sprite = batterySpriteLevels[2];
+        }
+        else if (grapplingHook.GetCurrentCooldown() < (grapplingHook.GetCooldownLength() * (0.6f)))
+        {
+            sr.sprite = batterySpriteLevels[1];
+        }
+        else if (grapplingHook.GetCurrentCooldown() < (grapplingHook.GetCooldownLength() * (0.8f)))
+        {
+            sr.sprite = batterySpriteLevels[0];
+        }
+        
+        
     }
 }
