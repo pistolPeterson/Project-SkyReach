c8f4ef6f99dc89338f963dfbe9d4b51900fa9230
jaelenedc <jdela003@gmail.com>
2022-11-20 22:09:37 -0500

    pete carrying jaelene.
    god forbid.

--- Assets/Scripts/UI/EndCreditsAnimation.cs
+++ Assets/Scripts/UI/EndCreditsAnimation.cs
@@ -1,24 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 /// <summary>
-/// make a summary for script dummy 
+/// return button appears at end credit anim event
 /// </summary>
 public class EndCreditsAnimation : MonoBehaviour
 {
     //reference to button gameobject 
-    
-
+    private GameObject returnButton;
+  
     // Start is called before the first frame update
     void Start()
     {
         //make button not active on start 
-        
+        returnButton.
         
     }
 
    
     
     //a method that sets the button gameobject active 
-    
+    public void activeButton
+    {
+
+    }
 }
