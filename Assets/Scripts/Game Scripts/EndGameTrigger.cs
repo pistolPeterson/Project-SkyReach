5ce9fac7d99f1c76b4abf77b78f4685fad0f9aa6
PetersonNormil <petersonnormil@gmail.com>
2022-12-02 07:50:11 -0500

    teleport sfx + end credits music

--- Assets/Scripts/Game Scripts/EndGameTrigger.cs
+++ Assets/Scripts/Game Scripts/EndGameTrigger.cs
@@ -6,10 +6,13 @@ using UnityEngine;
 
 public class EndGameTrigger : MonoBehaviour
 {
+    public AudioSource source;
+    public AudioClip teleportClip;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.attachedRigidbody == GameManager.Player.Body)
         {
+            source.PlayOneShot(teleportClip);
             GameManager.WinGame();
         }
     }
